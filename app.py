3c1efeecf6af39940bd51f9da1c0db153f39c7ca
felipesanni <felipesannino@hotmail.com>
2024-07-26 17:19:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
-import os
 
 app = Flask(__name__)
 
-@app.route('/api/estatisticas', methods=['GET'])
-def get_estatisticas():
-    url = "https://datajud-api.cnj.jus.br/v1/processos"
-    headers = {"Authorization": "Bearer seu_token_aqui"}
-    
+API_KEY = "cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw=="
+
+# Função para obter os dados de um endpoint específico da API Datajud
+def obter_dados_datajud(endpoint):
+    url = f"https://datajud-api.cnj.jus.br{endpoint}"
+    headers = {"Authorization": f"APIKey {API_KEY}"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        estatisticas = {
-            "processos_distribuidos": data["processos_distribuidos"],
-            "processos_julgados": data["processos_julgados"],
-            "processos_pendentes": data["processos_pendentes"]
-        }
-        return jsonify(estatisticas)
+        return response.json()
     else:
-        return jsonify({"error": "Não foi possível obter as estatísticas"}), response.status_code
+        return {"error": "Não foi possível obter os dados"}, response.status_code
+
+@app.route('/api/<path:endpoint>', methods=['GET'])
+def consulta_datajud(endpoint):
+    dados = obter_dados_datajud(f"/v1/{endpoint}")
+    return jsonify(dados)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
